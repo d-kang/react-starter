c322374a469115325f8bc8b1fbfef9f2badfcd94
d-kang <david.kang714@gmail.com>
2017-10-11 08:14:51 -0700

    Init React CDNs and Babel Presets

---
+++ index.js
@@ -0,0 +1,21 @@
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+
+  myStr = 'counter';
+
+  addCount = () => this.setState({ count: this.state.count += 1 });
+
+  render = () =>
+    <div>
+      <div>{this.myStr + ' ' + this.state.count}</div>
+      <input
+        type="button"
+        value='button'
+        onClick={this.addCount}
+      />
+    </div>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
