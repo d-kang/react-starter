4c42bf9b510f4f47b3df55e3e9212c82d1feb857
d-kang <david.kang714@gmail.com>
2017-10-30 15:59:33 -0700

    Add in App Component that Renders Welcome message and Hello Component

---
+++ react_webpack_typescript/src/components/App.tsx
@@ -0,0 +1,21 @@
+import * as React from "react";
+
+import Hello from "./Hello";
+
+export interface AppProps { welcome: string; }
+
+// 'AppProps' describes the shape of props.
+// State is never set so we use the '{}' type.
+class App extends React.Component<AppProps, {}> {
+  render() {
+    return (
+      <div>
+        {this.props.welcome} <br />
+        <Hello compiler="TypeScript" framework="React" />
+      </div>
+
+    );
+  }
+}
+
+export default App
--- react_webpack_typescript/src/components/hello.tsx
+++ react_webpack_typescript/src/components/hello.tsx
@@ -4,8 +4,11 @@ export interface HelloProps { compiler: string; framework: string; }
 
 // 'HelloProps' describes the shape of props.
 // State is never set so we use the '{}' type.
-export class Hello extends React.Component<HelloProps, {}> {
+class Hello extends React.Component<HelloProps, {}> {
   render() {
     return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
   }
 }
+
+
+export default Hello;
